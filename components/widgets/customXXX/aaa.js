9732fcb985bcecf42208b88dde9f650284823abc
Rin Nguyen <rinnguyen@kms-technology.com>
2017-11-16 21:14:42 +0700

    update code

--- components/widgets/customXXX/aaa.js
+++ components/widgets/customXXX/aaa.js
@@ -26,33 +26,53 @@ export default class Test extends Component {
     loading: true
   }
 
-  getDefaultProps () {
-    return {
-      interval: 1000,
-      titlex: '123 XXX'
-    }
-  }
-
-  getInitialState () {
-    return {
-      count: 0,
-      error: false,
-      loading: true
-    }
-  }
-
   componentWillUnmount () {
     clearTimeout(this.timeout)
   }
 
   render () {
     const {count, error, loading} = this.state
-    const {titlex} = this.props
+    const {titlex: title} = this.props
+    this.alertxx('messxxx')
     return (
-      <Widget title={titlex} loading={loading} error={error}>
-        <Counter value={count}/>
-      </Widget>
+      <div>
+        <Widget title={title} loading={loading} error={error}>
+          <Counter value={count}/>
+        </Widget>
+        <button className='button' onClick={this.getNameFromAI()}>
+          Click Me
+        </button>
+      </div>
     )
+  }
+
+  async getName (aaa) {
+    const {authKey, owner, repository} = this.props
+    const opts = authKey ? {headers: basicAuthHeader(authKey)} : {}
+    const res = await fetch(`https://api.github.com/repos/${owner}/${repository}`, opts)
+    const json = await res.json()
+    return json
+  }
+
+  getNameFromAI = () => {
+    console.log('Test')
+    // let xxx = this.getName('abc')
+    //   .then(res => {
+    //     return res.json().name
+    //   })
+    //   .catch(err => {
+    //     return ''
+    //   })
+    // console.log(xxx)
+  }
+
+  increaseCount = () => {
+    const {count: time} = this.state
+    this.setState({count: time + 1})
+  }
+
+  alertxx = (mess) => {
+    // window.alert(mess)
   }
 
   componentDidMount () {
@@ -75,8 +95,9 @@ export default class Test extends Component {
       this.setState({count: json.open_issues_count, error: false, loading: false})
     } catch (error) {
       this.setState({error: true, loading: false})
-    } finally {
-      this.timeout = setTimeout(() => this.fetchInformation(), this.props.interval)
     }
+    // finally {
+    //    this.timeout = setTimeout(() => this.fetchInformation(), this.props.interval)
+    // }
   }
 }
--- pages/team-unicorn.js
+++ pages/team-unicorn.js
@@ -3,6 +3,7 @@ import DateTime from '../components/widgets/datetime'
 import lightTheme from '../styles/light-theme'
 import Test from '../components/widgets/customXXX/aaa'
 import * as React from 'react'
+import Select from '../components/widgets/customXXX/bbb'
 
 export default () => (
   <Dashboard theme={lightTheme}>
@@ -10,6 +11,9 @@ export default () => (
     <Test
       owner='danielbayerlein'
       repository='dashboard'
+    />
+    <Select
+      values={['State.', 'Should.', 'Be.', 'Synchronous.']}
     />
     <DateTime interval={1000} />
   </Dashboard>
